636cedceaabfc2e649ee88ff1a6ccea64ddb9c1d
vivekgandharla <sonugandherla01@gmail.com>
2025-07-10 11:25:33 +0530

    Fix input handling in calculator program and improve user prompts

--- project2.js
+++ project2.js
@@ -2,8 +2,24 @@
 const prompt = require("prompt-sync")()
 
 //Calculator
-const num1 = parseFloat(prompt("Enter the First num : "))
-const num2 = parseFloat(prompt("Enter the Second num : "))
+let num1;
+let num2;
+while(true){
+    num1 = parseFloat(prompt("Enter the First num : "))
+    if(isNaN(num1)){
+        console.log("Invalid Input")
+    }else{
+        break
+    }
+}
+while(true){
+    3num2 = parseFloat(prompt("Enter the Second num : "))
+    if(isNaN(num2)){
+        console.log("Invalid input")
+    }else{
+        break
+    }
+}
 const operator = prompt("Enter sign : ")
 
 let result;
